fcae8bc0ad5fdae897b0f7a3bf8a51bd330f2ea4
thoompranay-maker <thoompranay@gmail.com>
2026-02-13 11:22:11 +0530

    Import modules and render pregnancy dashboard

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 
+import { LMP_DATE } from "./config.js";
+import { calculatePregnancyData } from "./pregnancyEngine.js";
+import { renderDashboard } from "./uiRenderer.js";
+
+const pregnancyData = calculatePregnancyData(LMP_DATE);
+renderDashboard(pregnancyData);
