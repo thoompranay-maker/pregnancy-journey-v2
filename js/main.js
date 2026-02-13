075ce47719bc637e7835b3a5d445936802c2feba
thoompranay-maker <thoompranay@gmail.com>
2026-02-13 12:02:03 +0530

    Set background gradient based on trimester
    Change background color based on pregnancy trimester.

--- js/main.js
+++ js/main.js
@@ -5,3 +5,14 @@ import { renderDashboard } from "./uiRenderer.js";
 
 const pregnancyData = calculatePregnancyData(LMP_DATE);
 renderDashboard(pregnancyData);
+
+if (pregnancyData.trimester === "First Trimester") {
+    document.body.style.background =
+        "linear-gradient(to bottom, #fde2e4, #f8edeb)";
+} else if (pregnancyData.trimester === "Second Trimester") {
+    document.body.style.background =
+        "linear-gradient(to bottom, #e0c3fc, #8ec5fc)";
+} else {
+    document.body.style.background =
+        "linear-gradient(to bottom, #ffecd2, #fcb69f)";
+}
